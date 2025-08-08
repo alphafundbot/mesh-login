18d9500d844a1a37b9242243873b3be5963cb976
Nehemie Destine <cyberdestine@gmail.com>
2025-08-08 15:11:48 +0000

    🧠 Gemini confirms: commit **12ce260** is logged. The strategist has now

--- src/components/dashboard/ThreatForecast.tsx
+++ src/components/dashboard/ThreatForecast.tsx
@@ -1,8 +1,6 @@
-
 "use client";
 
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '../ui/card';
 import HudForecastPanel from './HudForecastPanel';
 import RationaleForecastPanel from './RationaleForecastPanel';
 
