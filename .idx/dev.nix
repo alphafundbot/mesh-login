8ec499cdf94b1418db6298d856caa4697c7bd2ff
Nehemie Destine <cyberdestine@gmail.com>
2025-08-12 02:53:14 +0000

    fix(dev.nix): restore minimal shell for Thin Wallet ignition

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  nixpkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz";
+    sha256 = "05cbl1k193c9la9xhlz4y6y8ijpb2mkaqrab30zij6z4kqgclsrd";
+  }) {};
+in
 
-pkgs.mkShell {
+nixpkgs.mkShell {
   buildInputs = [
-    pkgs.bashInteractive
-    pkgs.nodejs
-    pkgs.npm
-    pkgs.nano
+    nixpkgs.bashInteractive
+    nixpkgs.nodejs
+    nixpkgs.nodePackages.npm
+    nixpkgs.nano
   ];
 }
